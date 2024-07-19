6b96f5d9e4a694a88daca3f0d718b7e66afd3574
Anon2148 <privext@tutanota.com>
2024-07-19 23:48:35 +0200

    feat: exercises 2.4 and 2.5, courseinfo steps 9 and 10 done

--- courseinfo/src/Components/Course/Course.jsx
+++ courseinfo/src/Components/Course/Course.jsx
@@ -3,7 +3,6 @@ import Part from "./Part";
 
 const Course = ({ course }) => {
   const total = course.parts.reduce((s, p) => {
-    console.log("what is happening", s, p);
     return s + p.exercises;
   }, 0);
 
--- courseinfo/src/Components/Course/Title.jsx
+++ courseinfo/src/Components/Course/Title.jsx
@@ -1,5 +1,5 @@
 const Title = ({ name }) => {
-  return <h1>{name}</h1>;
+  return <h2>{name}</h2>;
 };
 
 export default Title;
