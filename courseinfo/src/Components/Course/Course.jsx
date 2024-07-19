0d0375cabbde639df53b85d8673026097d074ded
Anon2148 <privext@tutanota.com>
2024-07-19 23:28:13 +0200

    feat: exercise 2.2, courseinfo step 7 done

--- courseinfo/src/Components/Course/Course.jsx
+++ courseinfo/src/Components/Course/Course.jsx
@@ -2,12 +2,21 @@ import Title from "./Title";
 import Part from "./Part";
 
 const Course = ({ course }) => {
+  const countAll = () => {
+    let sum = 0;
+    course.parts.forEach((part) => {
+      sum += part.exercises;
+    });
+    return sum;
+  };
+
   return (
     <>
       <Title name={course.name} />
       {course.parts.map((part) => (
         <Part key={part.id} name={part.name} exercises={part.exercises} />
       ))}
+      <b>total of {countAll()} exercises</b>
     </>
   );
 };
